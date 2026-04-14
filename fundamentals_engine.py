285f80c9745c35ffb340a4b65884b1a884160679
anveshmrao20-create <anveshmrao20@gmail.com>
2026-04-14 14:39:30 +0530

    Update fundamentals_engine.py

--- fundamentals_engine.py
+++ fundamentals_engine.py
@@ -49,7 +49,7 @@ def get_sheet():
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive"
     ]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("/opt/airflow/scripts/credentials.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
     client = gspread.authorize(creds)
     return client.open(SPREADSHEET_NAME).worksheet(SHEET_NAME)
 
